e65758a691e045e2a9f44899b714fd4568fbf7f7
Krisztian Bui <buikrisz00@gmail.com>
2022-03-02 22:46:14 +0100

    gallery modal done

--- frontend/gallery.js
+++ frontend/gallery.js
@@ -1,42 +1,87 @@
-async function loadEvent() {
-    let galleryElement = document.getElementById("gallery");
+/* Create variables for innerHTML values */
+let galleryTag = "";
+let fetchedData = {};
+const modalHTML = `<div class="modal"></div>`;
 
-    let fetchGallery = async function (x) {
+const generateModal = function (modalDiv, index) {
+    let imgShow = "";
+    let iframeShow = "";
 
-        let today = new Date();
-        let apiParams = {
-            start_date: "2022-02-01",
-            end_date: today.getFullYear().toString() + "-" + (today.getMonth()+1).toString().padStart(2, "0") + "-" + today.getDate().toString().padStart(2, "0")
-        }
-        let url = `https://api.nasa.gov/planetary/apod?api_key=MQDFPbyygp3ONA3J2zcHkIqLbdYEJcU0ss2MDhqH&start_date=${apiParams.start_date}&end_date=${apiParams.end_date}`
-    
-        /* Create variables for innerHTML values */
-        let galleryTag = "";
-    
-        
-        await fetch (url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                for (const item of data) {
-                    if (item.media_type === "image") {
-                        galleryTag += `<div><img src="${item.url}" alt="test image" class="galleryImage"></div>`;
-                    } else {
-                        galleryTag += `<div><iframe src="${item.url}" title="${item.title}" class="iframe"></iframe></div>`
-                    }
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-        
-        galleryElement.insertAdjacentHTML("beforeend", galleryTag);
+    // Check if video or image
+    if (fetchedData[index].media_type === "image") {
+        iframeShow = "noShow";
+    } else {
+        imgShow = "noShow";
     }
 
-    fetchGallery();
+    // Modal show
+    modalDiv.classList.add("showModal");
 
-    
+    return `
+    <div class="modal-content">
+        <h1 class="title">${fetchedData[index].title}</h1>
+        <div class="imageHolder">
+            <img src="${fetchedData[index].url}" alt="test image" class="${imgShow} image">
+            <iframe src="${fetchedData[index].url}" title="${fetchedData[index].title}" class="${iframeShow} iframe"></iframe>
+        </div>
+        <p class="date">(${fetchedData[index].date})</p>
+        <p class="explanation">${fetchedData[index].explanation}</p>
+    </div>
+    `
+}
+
+let fetchGallery = async function () {
+
+    let today = new Date();
+    let apiParams = {
+        start_date: "2022-02-01",
+        end_date: today.getFullYear().toString() + "-" + (today.getMonth()+1).toString().padStart(2, "0") + "-" + today.getDate().toString().padStart(2, "0")
+    }
+    let url = `https://api.nasa.gov/planetary/apod?api_key=MQDFPbyygp3ONA3J2zcHkIqLbdYEJcU0ss2MDhqH&start_date=${apiParams.start_date}&end_date=${apiParams.end_date}`
+
+    await fetch (url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            fetchedData = data;
+            for (const item of data) {
+                if (item.media_type === "image") {
+                    galleryTag += `<div><img src="${item.url}" alt="test image" class="galleryImage clickable"></div>`;
+                } else {
+                    galleryTag += `<div><iframe src="${item.url}" title="${item.title}" class="iframe clickable"></iframe></div>`
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+async function loadEvent() {
+    const galleryElement = document.getElementById("gallery");
+
+    // Fill up the galleryTag variable
+    await fetchGallery();
+    // Load content
+    galleryElement.insertAdjacentHTML("beforeend", galleryTag);
+    galleryElement.insertAdjacentHTML("beforeend", modalHTML);
+
+    // Create modal on click
+    const modalDiv = document.querySelector(".modal");
+
+    const galleryImages = galleryElement.querySelectorAll(".clickable");
+
+    for (let i = 0; i < galleryImages.length; i++) {
+        galleryImages[i].addEventListener("click", function () {
+            modalDiv.innerHTML = generateModal(modalDiv, i);
+        })
+    }
+
+    // Remove modal on click
+    modalDiv.addEventListener("click", function (event) {
+        event.target.classList.remove("showModal");
+    })
 
 }
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,165 +1,167 @@
+let landingPageLayout = "";
+let fetchedData = {};
+let todayNumber = 0;
+// Set API start- and end-date parameters
+const today = new Date();
+const apiParams = {
+    start_date: "2022-02-01",
+    end_date: today.getFullYear().toString() + "-" + (today.getMonth()+1).toString().padStart(2, "0") + "-" + today.getDate().toString().padStart(2, "0")
+}
+
+// Generate HTML function
+function generateHTML(header, img, date, explanation, media_type) {
+    let imgShow = "";
+    let iframeShow = "";
+    if (media_type === "image") {
+        iframeShow = "noShow";
+    } else {
+        imgShow = "noShow";
+    }
+    return `
+    <a class="galleryBtn" href="gallery.html">Gallery</a>
+    <form>
+        <label for="date">Pick a date:</label>
+        <input type="date" id="date" name="date">
+    </form>
+    <h1 class="title">${header}</h1>
+    <div class="imageHolder">
+        <img src="${img}" alt="test image" class="${imgShow} image">
+        <iframe src="${img}" title="${header}" class="${iframeShow} iframe"></iframe>
+    </div>
+    <p class="date">(${date})</p>
+    <p class="explanation">${explanation}</p>
+    <button id="left" class="leftButton">&#8592;</button>
+    <button id="right" class="rightButton">&#8594;</button>
+    `
+}
+
+// Fetch data function
+async function fetchFirst() {
+    let url = `https://api.nasa.gov/planetary/apod?api_key=MQDFPbyygp3ONA3J2zcHkIqLbdYEJcU0ss2MDhqH&start_date=${apiParams.start_date}&end_date=${apiParams.end_date}`;
+
+    await fetch (url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            landingPageLayout = generateHTML(data[data.length-1].title, data[data.length-1].url, data[data.length-1].date, data[data.length-1].explanation, data[data.length-1].media_type);
+            todayNumber = data.length - 1;
+            // For the click events
+            fetchedData = data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+    
+    
+    /* console.log(galleryElement);
+    galleryElement.insertAdjacentHTML("beforeend", galleryTag); */
+
+    
+}
+
+// Fetch single day for calendar
+const fetchSingleDay = function (h1, image, iframe, date, explanation, input) {
+    let requestedDate = input.target.value;
+    let url = `https://api.nasa.gov/planetary/apod?api_key=MQDFPbyygp3ONA3J2zcHkIqLbdYEJcU0ss2MDhqH&date=${requestedDate}`;
+
+    fetch (url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            h1.innerHTML = `${data.title}`;
+            if (data.media_type === "image") {
+                image.classList.remove("noShow");
+                iframe.classList.add("noShow");
+                image.src = `${data.url}`;
+            } else {
+                image.classList.add("noShow");
+                iframe.classList.remove("noShow");
+                iframe.src = `${data.url}`;
+            }
+            date.innerHTML = `(${data.date})`;
+            explanation.innerHTML = `${data.explanation}`;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
 async function loadEvent() {
     let rootElement = document.getElementById("root");
-    let galleryElement = document.getElementById("gallery");
+    /* let galleryElement = document.getElementById("gallery"); */
 
-    let fetchFirst = async function (x) {
+    // Fill up the landingPageLayout variable with data
+    await fetchFirst();
 
-        let today = new Date();
-        let apiParams = {
-            start_date: "2022-02-01",
-            end_date: today.getFullYear().toString() + "-" + (today.getMonth()+1).toString().padStart(2, "0") + "-" + today.getDate().toString().padStart(2, "0")
-        }
-        let url = `https://api.nasa.gov/planetary/apod?api_key=MQDFPbyygp3ONA3J2zcHkIqLbdYEJcU0ss2MDhqH&start_date=${apiParams.start_date}&end_date=${apiParams.end_date}`
-    
-        /* Create variables for innerHTML values */
-        let headerTag = "";
-        let imgTag = "";
-        let dateTag = "";
-        let explanationTag = "";
-        let todayNumber = 0;
-        let fetchedData = {};
-        let galleryTag = "";
-    
-        
-        await fetch (url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                headerTag = `<h1 class="title">${data[data.length-1].title}</h1>`;
-                if (data[data.length-1].media_type === "image") {
-                    imgTag = `
-                    <img src="${data[data.length-1].url}" alt="test image" class="image">
-                    <iframe src="${data[data.length-1].url}" title="${data[data.length-1].title}" class="noShow iframe"></iframe>
-                    `;
+    // Insert landingPageLayout into HTML file
+    rootElement.insertAdjacentHTML("beforeend", landingPageLayout);
+
+    /* Get HTML elements */
+    let leftButton = rootElement.querySelector(".leftButton");
+    let rightButton = rootElement.querySelector(".rightButton");
+    let h1 = rootElement.querySelector(".title");
+    let iframe = rootElement.querySelector(".iframe");
+    let image = rootElement.querySelector(".image");
+    let date = rootElement.querySelector(".date");
+    let explanation = rootElement.querySelector(".explanation");
+    let counter = todayNumber;
+
+    const clickedButton = function (buttonId) {
+        if (buttonId.target.id === "left") {
+            if (counter >= 1) {
+                h1.innerHTML = `${fetchedData[counter - 1].title}`;
+                if (fetchedData[counter - 1].media_type === "image") {
+                    image.classList.remove("noShow");
+                    iframe.classList.add("noShow");
+                    image.src = `${fetchedData[counter - 1].url}`;
                 } else {
-                    imgTag = `
-                    <img src="${data[data.length-1].url}" alt="test image" class="noShow image">
-                    <iframe src="${data[data.length-1].url}" title="${data[data.length-1].title}" class="iframe"></iframe>
-                    `;;
+                    image.classList.add("noShow");
+                    iframe.classList.remove("noShow");
+                    iframe.src = `${fetchedData[counter - 1].url}`;
                 }
-                dateTag = `<p class="date">(${data[data.length-1].date})</p>`;
-                explanationTag = `<p class="explanation">${data[data.length-1].explanation}</p>`;
-                todayNumber = data.length - 1;
-                fetchedData = data;
-                /* for (const item of data) {
-                    galleryTag += `<img src="${item.url}" alt="test image" class="galleryImage">`;
-                } */
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    
-        
-        let landingPageLayout = `
-            <a class="galleryBtn" href="gallery.html">Gallery</a>
-            <form>
-                <label for="date">Pick a date:</label>
-                <input type="date" id="date" name="date">
-            </form>
-            ${headerTag}
-            <div class="imageHolder">
-                ${imgTag + dateTag}
-            </div>
-            ${explanationTag}
-            <button id="left" class="leftButton">&#8592;</button>
-            <button id="right" class="rightButton">&#8594;</button>
-        `;
-        
-        rootElement.insertAdjacentHTML("beforeend", landingPageLayout);
-        /* console.log(galleryElement);
-        galleryElement.insertAdjacentHTML("beforeend", galleryTag); */
-    
-        /* Get HTML elements */
-        let leftButton = rootElement.querySelector(".leftButton");
-        let rightButton = rootElement.querySelector(".rightButton");
-        let h1 = rootElement.querySelector(".title");
-        let iframe = rootElement.querySelector(".iframe");
-        let image = rootElement.querySelector(".image");
-        let date = rootElement.querySelector(".date");
-        let explanation = rootElement.querySelector(".explanation");
-        let counter = todayNumber;
-        
-        let clickedButton = function (buttonId) {
-            if (buttonId.target.id === "left") {
-                if (counter >= 1) {
-                    h1.innerHTML = `${fetchedData[counter - 1].title}`;
-                    if (fetchedData[counter - 1].media_type === "image") {
-                        image.classList.remove("noShow");
-                        iframe.classList.add("noShow");
-                        image.src = `${fetchedData[counter - 1].url}`;
-                    } else {
-                        image.classList.add("noShow");
-                        iframe.classList.remove("noShow");
-                        iframe.src = `${fetchedData[counter - 1].url}`;
-                    }
-                    date.innerHTML = `(${fetchedData[counter - 1].date})`;
-                    explanation.innerHTML = `${fetchedData[counter - 1].explanation}`;
-                    
-                    counter--;
+                date.innerHTML = `(${fetchedData[counter - 1].date})`;
+                explanation.innerHTML = `${fetchedData[counter - 1].explanation}`;
+                
+                counter--;
+            }
+        } else {
+            if (counter < todayNumber) {
+                counter++;
+
+                h1.innerHTML = `${fetchedData[counter].title}`;
+                if (fetchedData[counter].media_type === "image") {
+                    image.classList.remove("noShow");
+                    iframe.classList.add("noShow");
+                    image.src = `${fetchedData[counter].url}`;
+                } else {
+                    image.classList.add("noShow");
+                    iframe.classList.remove("noShow");
+                    iframe.src = `${fetchedData[counter].url}`;
                 }
-            } else {
-                if (counter < todayNumber) {
-                    counter++;
-    
-                    h1.innerHTML = `${fetchedData[counter].title}`;
-                    if (fetchedData[counter].media_type === "image") {
-                        image.classList.remove("noShow");
-                        iframe.classList.add("noShow");
-                        image.src = `${fetchedData[counter].url}`;
-                    } else {
-                        image.classList.add("noShow");
-                        iframe.classList.remove("noShow");
-                        iframe.src = `${fetchedData[counter].url}`;
-                    }
-                    date.innerHTML = `(${fetchedData[counter].date})`;
-                    explanation.innerHTML = `${fetchedData[counter].explanation}`;
-                }
+                date.innerHTML = `(${fetchedData[counter].date})`;
+                explanation.innerHTML = `${fetchedData[counter].explanation}`;
             }
         }
-        leftButton.addEventListener("click", function (x) {
-            clickedButton(x)
-        })
-        rightButton.addEventListener("click", function (x) {
-            clickedButton(x)
-        })
-    
-        let input = rootElement.querySelector("#date");
-        input.value = apiParams.end_date;
-    
-        let fetchSingleDay = function (input) {
-            let requestedDate = input.target.value;
-            let url = `https://api.nasa.gov/planetary/apod?api_key=MQDFPbyygp3ONA3J2zcHkIqLbdYEJcU0ss2MDhqH&date=${requestedDate}`;
-    
-            fetch (url)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    h1.innerHTML = `${data.title}`;
-                    if (data.media_type === "image") {
-                        image.classList.remove("noShow");
-                        iframe.classList.add("noShow");
-                        image.src = `${data.url}`;
-                    } else {
-                        image.classList.add("noShow");
-                        iframe.classList.remove("noShow");
-                        iframe.src = `${data.url}`;
-                    }
-                    date.innerHTML = `(${data.date})`;
-                    explanation.innerHTML = `${data.explanation}`;
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-        }
-    
-        input.addEventListener("input", function (x) {
-            fetchSingleDay(x);
-        })
     }
-    fetchFirst();
 
+    leftButton.addEventListener("click", function (x) {
+        clickedButton(x)
+    });
+
+    rightButton.addEventListener("click", function (x) {
+        clickedButton(x)
+    });
+
+    // Calendar picker
+    const input = rootElement.querySelector("#date");
+    input.value = apiParams.end_date;
+
+    input.addEventListener("input", function (input) {
+        fetchSingleDay(h1, image, iframe, date, explanation, input);
+    })
 }
 
 window.addEventListener("load", loadEvent);
